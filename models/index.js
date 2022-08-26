7b052818083c0dbcc55778b5c3f803ad275824ef
DRCallaghan <dennis.callaghan87@gmail.com>
2022-08-26 17:19:15 -0400

    added model associations

--- models/index.js
+++ models/index.js
@@ -5,12 +5,30 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  // defining the third table needed to store foreign keys
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+});
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  // defining the third table needed to store foreign keys
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+});
 
 module.exports = {
   Product,
